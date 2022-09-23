6658d8a1b4accf0181594ef5c7049fd98d39d404
TamayozAcademy <112080161+TamayozAcademy@users.noreply.github.com>
2022-09-23 22:20:24 +0200

    Update object.js

--- js/object.js
+++ js/object.js
@@ -1,5 +1,13 @@
 var object = [
  
- 
+  
+{
+"Your_Name_Is":"Ahmed Elkady",
+"Your_Code_Is":"1",
+"Department":"Kadysoft CEO",
+"_source":{
+"Cer_Link":"https://scontent.fcai20-5.fna.fbcdn.net/v/t1.6435-9/120318418_190324815943346_6107111893154541051_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_ohc=SOLS7pMizXEAX9BS8dk&_nc_ht=scontent.fcai20-5.fna&oh=00_AT_L3uMh_Q50p_lu5KUVPp_WlY1bJdff0jIMqb-MZXdj8w&oe=63551191"
+}
+},
  
 ];
