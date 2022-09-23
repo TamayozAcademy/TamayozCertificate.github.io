775d36d8e9d705b60fb7abe6f80903031402b1c9
TamayozAcademy <112080161+TamayozAcademy@users.noreply.github.com>
2022-09-23 21:04:47 +0200

    Update object.js

--- js/object.js
+++ js/object.js
@@ -6,7 +6,7 @@ var object = [
 {
 "Your_Name_Is":"Ahmed Elkady",
 "Your_Code_Is":"12345",
-"Department":"ICDL Teacher",
+"Department":"Developer",
 "_source":{
 "Cer_Link":"https://scontent.fcai20-5.fna.fbcdn.net/v/t1.18169-9/17951509_1894584614117543_6166161847528864122_n.jpg?stp=dst-jpg_p526x296&_nc_cat=110&ccb=1-7&_nc_sid=9267fe&_nc_ohc=4jIYh0AgkUoAX_Hwf4I&_nc_oc=AQn3fQGx3goevouAJWMGnH6zs0sMZ7bid6OUqBD9JRgVUdl5REeU9E_OEssSfAjLa1g&_nc_ht=scontent.fcai20-5.fna&oh=00_AT_IjkNfZxATU2iRcnTo7YyK7KyH6WnTkIu8aYQHGcSuNw&oe=635359F6"
 }
