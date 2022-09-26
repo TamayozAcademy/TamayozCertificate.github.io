d0fc9d70f67669f4640836c0f7c5933c1b684dfb
TamayozAcademy <112080161+TamayozAcademy@users.noreply.github.com>
2022-09-26 16:44:52 +0200

    Update object.js

--- js/object.js
+++ js/object.js
@@ -1,13 +1,14 @@
 var object = [
  
-  
+   
 {
-"Your_Name_Is":"Ahmed Elkady",
-"Your_Code_Is":"1",
-"Department":"Kadysoft CEO",
+"Your_Name_Is":"hamza",
+"Your_Code_Is":"100",
+"Department":"nurse",
 "_source":{
-"Cer_Link":"https://scontent.fcai20-5.fna.fbcdn.net/v/t1.6435-9/120318418_190324815943346_6107111893154541051_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_ohc=SOLS7pMizXEAX9BS8dk&_nc_ht=scontent.fcai20-5.fna&oh=00_AT_L3uMh_Q50p_lu5KUVPp_WlY1bJdff0jIMqb-MZXdj8w&oe=63551191"
+"Cer_Link":"https://scontent.fcai26-1.fna.fbcdn.net/v/t39.30808-6/309206847_819657842720880_216689400536991847_n.jpg?stp=dst-jpg_p843x403&_nc_cat=101&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=q1vCqBP-JYwAX9cLngR&_nc_ht=scontent.fcai26-1.fna&oh=00_AT-BoU4Mwled_Dl7qa00uez2rk2Lnp4nW5SFuUau_f6wdQ&oe=6335C9DF"
 }
 },
+ 
  
 ];
